72c25b38428bb542305cafdf8f88107120f574e8
Satyender <s.satyender45@gmail.com>
2025-08-08 21:42:27 +0530

    Final working script

--- create_folder_v2.0.py
+++ create_folder_v2.0.py
@@ -1,6 +1,9 @@
 #!/usr/local/bin/python3.13
 import os
 
+ch_no_counter = 0
+ch_name_counter = 0
+
 path = "/home/satyender/Documents/pythonScripts/WF_1007"
 mode = 0o777
 client_code = input("Enter client code: ").upper()
@@ -16,10 +19,11 @@ chapter_name = ["Null Name" for x in chapter_num]
 '''
 
 n = int(input("Enter total number of chapters: "))
-chapter_dict = {input(f"{i+1}). chapter number: "):input(f"{i+1}). chapter name: ") for i in range(n)}
+chapter_dict = [[input(f"{i+1}). chapter number: "),input(f"{i+1}). chapter name: ")] for i in range(n)]
 
-chapter_dict.setdefault("Null","Null Name")
+# chapter_dict.setdefault("Null","Null Name")
 print(chapter_dict)
+
 
 for x in permanent_folders:
     try:
@@ -29,11 +33,20 @@ for x in permanent_folders:
     except OSError as err:
         print("Error in creating directory: "+str(err))
 
-for x,y in zip(chapter_dict, chapter_dict.values()):
+for x in chapter_dict:
     try:
-        final_path = os.path.join(path,prefix+"Chapter "+x.title()+"_"+y.title())
+        if x[0] == "":
+            x[0] = "Null"
+            if ch_no_counter>0:
+                x[0] = f"Null {ch_no_counter}"
+            ch_no_counter = ch_no_counter + 1
+        if x[1] == "":
+            x[1] = "Null Name"
+            if ch_name_counter>0:
+                x[1] = f"Null Name {ch_name_counter}"
+            ch_name_counter = ch_name_counter + 1
+        final_path = os.path.join(path,prefix+"Chapter "+x[0]+"_"+x[1])
         os.makedirs(final_path,mode)
         print("Directory Created")
     except OSError as err:
         print("Error in creating directory: "+str(err))   
-
