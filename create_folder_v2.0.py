db02e249f517510cc015088fab5fe748fdb514a5
Satyender <s.satyender45@gmail.com>
2025-08-08 23:10:49 +0530

    final v 2.0

--- create_folder_v2.0.py
+++ create_folder_v2.0.py
@@ -7,21 +7,14 @@ ch_name_counter = 0
 path = "/home/satyender/Documents/pythonScripts/WF_1007"
 mode = 0o777
 client_code = input("Enter client code: ").upper()
-book_name = input("Enter book name: ").title()
+book_name = input("Enter your book name: ").title()
 prefix = client_code +"_"+ book_name +"_"
-permanent_folders = ["cover","front_index","Back_Index","eplilogue"]
+permanent_folders = ["Cover","Front_Index","Back_Index","Eplilogue","Prologue","Acknowledgement"]
 
-'''
-chapter_num = [i for i in input("Enter chapter number: ").split(",")]
-print(chapter_num)
 
-chapter_name = ["Null Name" for x in chapter_num]
-'''
+n = int(input("Enter total number of chapters in book: "))
+chapter_dict = [[input(f"{i+1}). Chapter Number: "),input(f"{i+1}). Chapter's Name: ")] for i in range(n)]
 
-n = int(input("Enter total number of chapters: "))
-chapter_dict = [[input(f"{i+1}). chapter number: "),input(f"{i+1}). chapter name: ")] for i in range(n)]
-
-# chapter_dict.setdefault("Null","Null Name")
 print(chapter_dict)
 
 
@@ -40,13 +33,22 @@ for x in chapter_dict:
             if ch_no_counter>0:
                 x[0] = f"Null {ch_no_counter}"
             ch_no_counter = ch_no_counter + 1
+        
+        if x[0].find("-"):
+            x[0] = x[0].replace("-","  ")
+        
         if x[1] == "":
             x[1] = "Null Name"
             if ch_name_counter>0:
                 x[1] = f"Null Name {ch_name_counter}"
             ch_name_counter = ch_name_counter + 1
-        final_path = os.path.join(path,prefix+"Chapter "+x[0]+"_"+x[1])
+        
+        if x[1].find("-") :
+            x[1] = x[1].replace("-","  ")
+        
+        final_path = os.path.join(path,prefix+"Chapter "+x[0].title()+"_"+x[1].title())
         os.makedirs(final_path,mode)
         print("Directory Created")
+    
     except OSError as err:
         print("Error in creating directory: "+str(err))   
