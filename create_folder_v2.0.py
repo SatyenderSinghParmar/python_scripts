9463d5b0cf24026c87f1829ddb22df0efeed2d79
Satyender <s.satyender45@gmail.com>
2025-08-11 21:22:54 +0530

    final v 4.0

--- create_folder_v2.0.py
+++ create_folder_v2.0.py
@@ -4,16 +4,23 @@ import os
 ch_no_counter = 0
 ch_name_counter = 0
 
-path = "/home/satyender/Documents/pythonScripts/WF_1007"
+folder_path = input("Enter folder path: ")
 mode = 0o777
 client_code = input("Enter client code: ").upper()
 book_name = input("Enter your book name: ").title()
+path = folder_path+"/"+client_code+"_"+book_name
 prefix = client_code +"_"+ book_name +"_"
 permanent_folders = ["Cover","Front Index","Back Index","Eplilogue","Prologue","Acknowledgements"]
 
+no_add_permanent_folder = int(input("Enter no of extra folders: "))
+n_permanent_folders = []
+n_permanent_folders = [input(f"{i+1}). Enter additional folder name: ") for i in range(no_add_permanent_folder)]
+
+permanent_folders.extend(n_permanent_folders)
 
 n = int(input("Enter total number of chapters in book: "))
 chapter_dict = [[input(f"{i+1}). Chapter Number: "),input(f"{i+1}). Chapter's Name: ")] for i in range(n)]
+
 chapter_no_dict = {}
 
 print(chapter_dict)
@@ -64,7 +71,19 @@ for x in chapter_dict:
 
         if x[1].find("-") :
             x[1] = x[1].replace("-","  ")
-        
+
+        if x[1].find("#"):
+            x[1] = x[1].replace("#","  ")
+     
+        if x[1].find("$"):
+            x[1] = x[1].replace("$","  ")
+
+        if x[1].find("@"):
+            x[1] = x[1].replace("@","  ")
+
+        if x[1].find(","):
+            x[1] = x[1].replace(",","  ")
+
         final_path = os.path.join(path,prefix+"Chapter "+x[0].title()+"_"+x[1].title())
         os.makedirs(final_path,mode)
         print("Directory Created")
